7e1790bda965babbd88f6882db765aeba57f918e
Grigalgol <grisha101202@yandex.ru>
2022-02-15 20:11:46 +0300

    fixed task3

--- src/task3/Main.java
+++ src/task3/Main.java
@@ -1,26 +1,60 @@
 package task3;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) throws InterruptedException {
+
+        Set<String> hashSet = new HashSet<>();
         MySet mySet = new MySet();
-        mySet.add("Danya");
-        mySet.add("Grisha");
-        mySet.add("Vovan");
-        System.out.println(mySet);
-        System.out.println(mySet.size());
-        mySet.clear();
-        System.out.println(mySet.size());
 
-        System.out.println("=========================================");
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i<5000; i++) {
+                hashSet.add("i = " + i);
+                mySet.add("i = " + i);
+            }
+        });
+        Thread thread2 = new Thread(() -> {
+            for (int i = 5000; i<10000; i++) {
+                hashSet.add("i = " + i);
+                mySet.add("i = " + i);
+            }
+        });
 
+        thread1.start();
+        thread2.start();
+        Thread.sleep(3000);
+
+        System.out.println("Set count - " + hashSet.size());
+        System.out.println("MySet count - " + mySet.size());
+
+
+        List<String> arrayList = new ArrayList<>();
         MyList myList = new MyList();
-        myList.add("Vk");
-        myList.add("YouTube");
-        myList.add("Twitter");
-        System.out.println(myList);
 
-        myList.remove("YouTube");
-        System.out.println(myList);
-        System.out.println(myList.size());
+        Thread thread11 = new Thread(() -> {
+           for (int i = 0; i<5000; i++) {
+               arrayList.add("i + " + i);
+               myList.add("i + " + i);
+           }
+        });
+
+        Thread thread12 = new Thread(() -> {
+            for (int i = 5000; i<10000; i++) {
+                arrayList.add("i + " + i);
+                myList.add("i + " + i);
+            }
+        });
+
+        thread11.start();
+        thread12.start();
+        Thread.sleep(3000);
+        System.out.println();
+        System.out.println("List count - " + arrayList.size());
+        System.out.println("MyList count - " + myList.size());
+
     }
 }
--- src/task3/MySet.java
+++ src/task3/MySet.java
@@ -1,34 +1,192 @@
 package task3;
 
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 import java.util.concurrent.Semaphore;
 
-public class MySet {
-    private HashSet<String> hashSet;
+public class MySet implements Set<String> {
+    private Set<String> hashSet =  new HashSet<>();
     private Semaphore sem = new Semaphore(1);
 
-
-    public MySet() {
-        hashSet = new HashSet<>();
+    @Override
+    public boolean remove(Object o) {
+        try {
+            sem.acquire();
+            return hashSet.remove(o);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        finally {
+            sem.release();
+            return false;
+        }
     }
 
-    public void add(String s) throws InterruptedException {
-        sem.acquire();
-        hashSet.add(s);
-        sem.release();
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        try {
+            sem.acquire();
+            return hashSet.containsAll(c);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        finally {
+            sem.release();
+            return false;
+        }
     }
 
-    public int size() throws InterruptedException {
-        sem.acquire();
-        int size = hashSet.size();
-        sem.release();
-        return size;
+    @Override
+    public boolean addAll(Collection<? extends String> c) {
+        try {
+            sem.acquire();
+            return hashSet.addAll(c);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        finally {
+            sem.release();
+            return false;
+        }
     }
 
-    public void clear() throws InterruptedException {
-        sem.acquire();
-        hashSet.clear();
-        sem.release();
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        try {
+            sem.acquire();
+            return hashSet.retainAll(c);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        finally {
+            sem.release();
+            return false;
+        }
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        try {
+            sem.acquire();
+            return hashSet.removeAll(c);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        finally {
+            sem.release();
+            return false;
+        }
+    }
+
+    @Override
+    public void clear() {
+        try {
+            sem.acquire();
+            hashSet.clear();
+            sem.release();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public int size() {
+        try {
+            sem.acquire();
+            return hashSet.size();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        finally {
+            sem.release();
+            return hashSet.size();
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        try {
+            sem.acquire();
+            return hashSet.isEmpty();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        finally {
+            sem.release();
+            return true;
+        }
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        try {
+            sem.acquire();
+            return hashSet.contains(o);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        finally {
+            sem.release();
+            return false;
+        }
+    }
+
+    @Override
+    public Iterator<String> iterator() {
+        try {
+            sem.acquire();
+            return hashSet.iterator();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        finally {
+            sem.release();
+            return null;
+        }
+    }
+
+    @Override
+    public Object[] toArray() {
+        try {
+            sem.acquire();
+            return hashSet.toArray();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        finally {
+            sem.release();
+            return null;
+        }
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        try {
+            sem.acquire();
+            return hashSet.toArray(a);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        finally {
+            sem.release();
+            return null;
+        }
+    }
+
+    @Override
+    public boolean add(String s) {
+        try {
+            sem.acquire();
+            return hashSet.add(s);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        finally {
+            sem.release();
+            return false;
+        }
     }
 
     @Override
